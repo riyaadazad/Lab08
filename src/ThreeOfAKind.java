f78ccc65976dff1fa1c913f784c57493afd1439c
Riyaad Azad <azad.riyaad@gmail.com>
2019-10-25 14:26:48 +0000

    Update

---
+++ src/ThreeOfAKind.java
@@ -0,0 +1,8 @@
+public class ThreeOfAKind {
+
+    public static void main(String[] args) {
+        Die sixSidedDie = new Die(6);
+        Die fiveSidedDie = new Die(5);
+        Die fourSidedDie = new Die(4);
+    }
+}
